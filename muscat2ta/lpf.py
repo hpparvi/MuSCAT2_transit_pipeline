d72f2568c8156a11d67aa54b53cc12459305887a
Hannu Parviainen <hannu@iac.es>
2018-02-22 15:17:11 +0000

    Transit fitting update.

--- muscat2ta/lpf.py
+++ muscat2ta/lpf.py
@@ -3,13 +3,15 @@ import math as mt
 from os.path import join, split
 
 from numpy import (inf, sqrt, ones, hstack, zeros_like, median, floor, concatenate, dot, diff, log, ones_like,
-                   percentile, clip, argsort, any)
+                   percentile, clip, argsort, any, array)
 from numpy.linalg import lstsq
 from numpy.random import uniform, normal
-from scipy.stats import norm as N, uniform as U, scoreatpercentile as sap
+from scipy.stats import norm as N, uniform as U, gamma as GM, scoreatpercentile as sap
 from scipy.optimize import minimize
 from tqdm import tqdm
 from astropy.stats import sigma_clip, mad_std
+
+from emcee import EnsembleSampler
 
 from george import GP
 from george.kernels import ExpKernel as EK, ExpSquaredKernel as ESK, ConstantKernel as CK, Matern32Kernel as M32
@@ -35,7 +37,8 @@ with warnings.catch_warnings():
 
 from scipy.stats import t as tdist
 
-from parameter import *
+from muscat2ta.parameter import *
+from muscat2ta.parameter import UniformPrior as U
 
 
 class BaseLPF:
@@ -51,7 +54,10 @@ class BaseLPF:
         self.times = self.datasets.btimes
         self.fluxes = self.datasets.bfluxes
         self.covariates = self.datasets.bcovariates
-        self.wn = self.datasets.bwn
+        self.wn = [median(wn) for wn in self.datasets.bwn]
+
+        self.de = None
+        self.sampler = None
 
         # Read the planet parameters from the OEC
         # ---------------------------------------
@@ -64,7 +70,7 @@ class BaseLPF:
         psystem = [
             GParameter('tc', 'zero_epoch', 'd', N(t0, tce), (-inf, inf)),
             GParameter('pr', 'period', 'd', N(p, 1e-5), (0, inf)),
-            GParameter('k2', 'area_ratio', 'A_s', U(*k2lims), (0, inf)),
+            GParameter('k2', 'area_ratio', 'A_s', GM(0.1), (1e-8, inf)),
             GParameter('rho', 'stellar_density', 'g/cm^3', U(0.1, 5.0), (0, inf)),
             GParameter('b', 'impact_parameter', 'R_s', U(0.0, 1.0), (0, 1))]
 
@@ -91,10 +97,11 @@ class BaseLPF:
         self.times = self.datasets.times
         self.fluxes = self.datasets.fluxes
         self.covariates = self.datasets.covariates
-        self.wn = self.datasets.wn
+        self.wn = [median(wn) for wn in self.datasets.wn]
 
     def create_pv_population(self, npop=50):
         pvp = self.ps.sample_from_prior(npop)
+        pvp[:,2] = uniform(1e-8, 0.2**2, size=npop)
         ldsl = self.ps.blocks[1].slice
         for i in range(pvp.shape[0]):
             pid = argsort(pvp[i, ldsl][::2])[::-1]
@@ -102,11 +109,10 @@ class BaseLPF:
             pvp[i, ldsl][1::2] = pvp[i, ldsl][1::2][pid]
         return pvp
 
+    def baseline(self, pv):
+        return ones(self.npb)
 
-    def compute_baseline(self, pv):
-        raise NotImplementedError
-
-    def compute_transit(self, pv):
+    def transit_model(self, pv):
         _a = as_from_rhop(pv[3], pv[1])
         if _a <= 1.:
             return [ones_like(f) for f in self.times]
@@ -120,10 +126,13 @@ class BaseLPF:
             fluxes.append(self.tm.evaluate(t, _k, _uv, pv[0], pv[1], _a, _i))
         return fluxes
 
-    def compute_lc_model(self, pv):
-        bl = self.compute_baseline(pv)
-        tm = self.compute_transit(pv)
+    def flux_model(self, pv):
+        bl = self.baseline(pv)
+        tm = self.transit_model(pv)
         return [b * t for b, t in zip(bl, tm)]
+
+    def residuals(self, pv):
+        return [fo - fm for fo, fm in zip(self.fluxes, self.flux_model(pv))]
 
     def lnprior(self, pv):
         return self.ps.lnprior(pv)
@@ -142,11 +151,25 @@ class BaseLPF:
     def __call__(self, pv):
         return self.lnposterior(pv)
 
-    def optimize_global(self, niter=200, npop=50):
-        self.de = DiffEvol(self.lnposterior, clip(self.ps.bounds, -1, 1), npop, maximize=True)
-        self.de._population[:, :] = self.create_pv_population(npop)
-        for _ in tqdm(self.de(niter), total=niter, desc='Differential evolution'):
+    def optimize_global(self, niter=200, npop=50, population=None, label='Global optimisation'):
+        if self.de is None:
+            self.de = DiffEvol(self.lnposterior, clip(self.ps.bounds, -1, 1), npop, maximize=True)
+            if population is None:
+                self.de._population[:, :] = self.create_pv_population(npop)
+            else:
+                self.de._population[:,:] = population
+        for _ in tqdm(self.de(niter), total=niter, desc=label):
             pass
+
+    def sample_mcmc(self, niter=500, thin=5, label='MCMC sampling'):
+        if self.sampler is None:
+            self.sampler = EnsembleSampler(self.de.n_pop, self.de.n_par, self.lnposterior)
+            pop0 = self.de.population
+        else:
+            pop0 = self.sampler.chains[:,-1,:]
+        for _ in tqdm(self.sampler.sample(pop0, iterations=niter, thin=thin), total=niter, desc=label):
+            pass
+
 
 class SimpleLPF(BaseLPF):
     def __init__(self, target, datasets, filters, nthreads=1):
@@ -165,26 +188,26 @@ class SimpleLPF(BaseLPF):
         self._slbl = ps.blocks[2].slice
         self._sler = ps.blocks[3].slices
 
-    def compute_baseline(self, pv):
+    def baseline(self, pv):
         return pv[self._slbl]
 
     def lnlikelihood(self, pv):
-        flux_m = self.compute_lc_model(pv)
+        flux_m = self.flux_model(pv)
         return sum([ll_normal_es(self.fluxes[i], flux_m[i], pv[self._sler[i]]) for i in range(self.npb)])
 
 
 class NormalLSqLPF(BaseLPF):
     """Log posterior function with least-squares baseline and normal likelihood"""
-    def compute_baseline(self, pv):
-        model_fluxes = self.compute_transit(pv)
+    def baseline(self, pv):
+        model_fluxes = self.transit_model(pv)
         for i, fm in enumerate(model_fluxes):
             coefs = lstsq(self.covariates[i], self.fluxes[i] / fm)[0]
             fbl = dot(self.covariates[i], coefs)
             model_fluxes[i] = fbl
         return model_fluxes
 
-    def compute_lc_model(self, pv):
-        model_fluxes = self.compute_transit(pv)
+    def flux_model(self, pv):
+        model_fluxes = self.transit_model(pv)
         for i, fm in enumerate(model_fluxes):
             coefs = lstsq(self.covariates[i], self.fluxes[i] / fm)[0]
             fbl = dot(self.covariates[i], coefs)
@@ -192,7 +215,7 @@ class NormalLSqLPF(BaseLPF):
         return model_fluxes
 
     def lnlikelihood(self, pv):
-        flux_m = self.compute_lc_model(pv)
+        flux_m = self.flux_model(pv)
         lnlike = 0.0
         for i in range(self.npb):
             lnlike += ll_normal_es(self.fluxes[i], flux_m[i], self.wn[i])
@@ -204,16 +227,14 @@ class StudentLSqLPF(NormalLSqLPF):
 
     def __init__(self, target, datasets, filters, nthreads=1, **kwargs):
         super().__init__(target, datasets, filters, nthreads, **kwargs)
-        #ps = self.ps
         self.ps.thaw()
         perr = [LParameter('et_{:d}'.format(i), 'error_df', '', U(1e-6, 1), bounds=(1e-6, 1)) for i in range(self.npb)]
         self.ps.add_lightcurve_block('error', 1, self.npb, perr)
         self.ps.freeze()
-        #self.ps = ps
         self._sler = self.ps.blocks[2].slices
 
     def lnlikelihood(self, pv):
-        flux_m = self.compute_lc_model(pv)
+        flux_m = self.flux_model(pv)
         lnlike = 0.0
         for i, sl in enumerate(self._sler):
             sigma, df = self.wn[i], 1. / pv[sl]
@@ -221,107 +242,51 @@ class StudentLSqLPF(NormalLSqLPF):
         return lnlike
 
 
-class LinearLPF(BaseLPF):
-    def __init__(self, target, datasets, filters, nthreads=1, **kwargs):
-        super().__init__(target, datasets, filters, nthreads, **kwargs)
+class GPLPF(BaseLPF):
+    def __init__(self, target, datasets, filters, nthreads=1):
+        super().__init__(target, datasets, filters, nthreads)
+        self.logwnvar = log(array(self.wn) ** 2)
+        self._create_kernel()
+        self.covariates = [cv[:, self.covids] for cv in self.covariates]
+        self.gps = [GP(self._create_kernel(),
+                       mean=1., fit_mean=True,
+                       white_noise=wn, fit_white_noise=False) for wn in self.logwnvar]
+        self.compute_gps()
+        self.de = None
 
-        pbl = concatenate([(LParameter('bc_{:d}'.format(i), 'baseline_constant', '', N(1, 1e-3), bounds=(-inf, inf)),
-                            LParameter('bt_{:d}'.format(i), 'l_time_coefficient', '', N(0, 1e-1), bounds=(-inf, inf)),
-                            LParameter('bx_{:d}'.format(i), 'l_airmass_coefficient', '', N(0, 1e-1),
-                                       bounds=(-inf, inf)))
-                           for i in range(self.npb)])
-        ps = self.ps
-        ps.thaw()
-        ps.add_lightcurve_block('baseline', 3, self.npb, pbl)
-        ps.freeze()
+    def compute_gps(self, hps=None):
+        if hps is not None:
+            [gp.set_parameter_vector(hp) for gp, hp in zip(self.gps, hps)]
+        [gp.compute(cv) for gp, cv in zip(self.gps, self.covariates)]
 
-        # Define the parameter slices
-        # ---------------------------
-        self._slbl = ps.blocks[2].slice
+    def optimize_hps(self, pv, i=0):
+        self.gphpres = []
+        self.gphps = []
+        for gp, residual in tqdm(zip(self.gps, self.residuals(pv)), total=self.npb,
+                                 desc='Optimizing GP hyperparameters'):
+            res = minimize(gp.nll, gp.get_parameter_vector(), (residual,),
+                           jac=gp.grad_nll, bounds=gp.get_parameter_bounds())
+            self.gphpres.append(res)
+            self.gphps.append(res.x)
 
-    def deterministic_baseline(self, pv):
-        fm = []
-        for i, (bc, bt, bx) in enumerate(pv[self._slbl].reshape([3, -1])):
-            fm.append(bc + bt * self.auxs[i][:, 1] + bx * self.auxs[i][:, 2])
-        return fm
+    def _create_kernel(self):
+        self.covids = (2, 3, 4, 5, 6)
+        logv = self.logwnvar.mean()
+        kernel_sk = LK(0, order=1, bounds=((None, None),), ndim=5, axes=0)
+        kernel_am = LK(0, order=1, bounds=((None, None),), ndim=5, axes=1)
+        kernel_xy = (CK(logv, bounds=((-18, -10),), ndim=5, axes=[2, 3])
+                     * ESK(10, metric_bounds=((0, 10),), ndim=5, axes=[2, 3]))
+        kernel_en = LK(0, order=1, bounds=((None, None),), ndim=5, axes=4)
+        self.kernels = kernel_sk, kernel_am, kernel_xy, kernel_en
+        return kernel_sk + kernel_am + kernel_xy + kernel_en
 
-    def compute_baseline(self, pv):
-        model_fluxes = self.compute_transit(pv)
-        base_fluxes = self.deterministic_baseline(pv)
-        for i, (fm, fbd) in enumerate(zip(model_fluxes, base_fluxes)):
-            coefs = lstsq(self.auxs[i][:, [0, 3, 4, 5]], self.fluxes[i] - (fm * fbd))[0]
-            fbl = dot(self.auxs[i][:, [0, 3, 4, 5]], coefs)
-            model_fluxes[i] = fbl + fbd
-        return model_fluxes
-
-    def compute_lc_model(self, pv):
-        model_fluxes = self.compute_transit(pv)
-        base_fluxes = self.deterministic_baseline(pv)
-        for i, (fm, fbd) in enumerate(zip(model_fluxes, base_fluxes)):
-            coefs = lstsq(self.auxs[i][:, [0, 3, 4, 5]], self.fluxes[i] - (fm * fbd))[0]
-            fbl = dot(self.auxs[i][:, [0, 3, 4, 5]], coefs)
-            model_fluxes[i] = fm * fbd + fbl
-        return model_fluxes
-
-    def lnlikelihood(self, pv):
-        flux_m = self.compute_lc_model(pv)
-        lnlike = 0.0
-        for i in range(self.npb):
-            lnlike += ll_normal_es(self.fluxes[i], flux_m[i], self.wn[i])
-        return lnlike
-
-
-class GPLPF(LinearLPF):
-    def __init__(self, target, datasets, filters, nthreads=1, **kwargs):
-        self.gps = None
-        super().__init__(target, datasets, filters, nthreads, **kwargs)
-
-        self.kernels = [
-            CK(log(1e-6), log([[1e-14, 1e-4]]), ndim=4, axes=[0]) * EK( 1.0, log([[0.01, 1e5]]), ndim=4, axes=[0]) +
-            CK(log(1e-6), log([[1e-14, 1e-4]]), ndim=4, axes=[1]) * ESK(1.0, log([[0.50, 1e5]]), ndim=4, axes=[1]) +
-            CK(log(1e-6), log([[1e-14, 1e-4]]), ndim=4, axes=[2]) * ESK(1.0, log([[0.50, 1e5]]), ndim=4, axes=[2]) +
-            CK(log(1e-6), log([[1e-14, 1e-4]]), ndim=4, axes=[3]) * ESK(1.0, log([[0.50, 1e5]]), ndim=4, axes=[3])
-            for i in range(self.npb)]
-        self.gps = [GP(self.kernels[i]) for i in range(len(filters))]
-
-    def _setup_arrays(self, masks=None):
-        super()._setup_arrays(masks)
-        if self.gps:
-            [gp.compute(aux[:, [1, 3, 4, 5]], wn) for gp, aux, wn in zip(self.gps, self.auxs, self.wn)]
-
-    def optimize_hps(self, pv):
-        fms = self.compute_transit(pv)
-        res = [fo - fm for fo, fm in zip(self.fluxes, fms)]
-        self.hps = hps = []
-        self.lsq = []
-        for i, gp in enumerate(self.gps):
-            b = lstsq(self.auxs[i], 1 + res[i])[0]
-            b[3:] = 0
-            self.lsq.append(b[:3])
-            r = res[i] - dot(self.auxs[i], b)
-            r -= r.mean()
-            hps.append(minimize(lambda pv: gp.nll(pv, r), gp.get_parameter_vector(),
-                                jac=lambda pv: gp.grad_nll(pv, r), bounds=gp.get_parameter_bounds()))
-            gp.set_parameter_vector(self.hps[-1].x)
-            gp.compute(self.auxs[i][:, [1, 3, 4, 5]], self.wn[i])
-
-    def compute_baseline(self, pv):
-        return self.deterministic_baseline(pv)
-
-    def compute_lc_model(self, pv):
-        bl = self.compute_baseline(pv)
-        tm = self.compute_transit(pv)
-        return [b * t for b, t in zip(bl, tm)]
+    def predict(self, pv):
+        self.compute_gps(self.gphps)
+        return [gp.predict(residuals, cv, return_cov=False)
+                for gp, residuals, cv in zip(self.gps, self.residuals(pv), self.covariates)]
 
     def lnlikelihood(self, pv):
-        flux_m = self.compute_lc_model(pv)
         lnlike = 0.0
-        for i, (fo, fm, gp) in enumerate(zip(self.fluxes, flux_m, self.gps)):
-            lnlike += gp.lnlikelihood(fo - fm)
+        for gp, residuals in zip(self.gps, self.residuals(pv)):
+            lnlike += gp.lnlikelihood(residuals)
         return lnlike
-
-    def create_pv_population(self, npop=50):
-        pvp = self.ps.sample_from_prior(npop)
-        k2est = (1 - sap(concatenate(self.fluxes), 10))
-        pvp[:, 2] = uniform(0.95 * k2est, 1.05 * k2est, pvp.shape[0])
-        return pvp
--- muscat2ta/transitanalysis.py
+++ muscat2ta/transitanalysis.py
@@ -1,21 +1,28 @@
 import numpy as np
+import pandas as pd
+import xarray as xa
 
 from pathlib import Path
+from time import strftime
 
 from tqdm import tqdm
 from matplotlib.pyplot import subplots, setp
-from numpy import array, arange, min, max, sqrt, inf, floor
+from numpy import array, arange, min, max, sqrt, inf, floor, diff, percentile, median
+from numpy.random import permutation
+from astropy.stats import mad_std
 
 from muscat2ph.phdata import PhotometryData
 from muscat2ta.lc import M2LCSet, M2LightCurve
-from muscat2ta.lpf import StudentLSqLPF
-from muscat2ta.gp import M2GP
+from muscat2ta.lpf import StudentLSqLPF, GPLPF
 
 class TransitAnalysis:
     pbs = 'g r i z_s'.split()
 
-    def __init__(self, datadir, target, tid, cids, fend=inf, nbins=500):
+    def __init__(self, datadir, target, tid, cids, fend=inf, etime=30.):
         self.ddata = dd = Path(datadir)
+        self.target = target
+        self.tid = tid
+        self.cids = cids
         self.phs = [PhotometryData(dd.joinpath('{}-{}.nc'.format(target, pb)), tid, cids, fend=fend)
                     for pb in self.pbs]
         [ph.select_aperture() for ph in self.phs]
@@ -25,70 +32,113 @@ class TransitAnalysis:
 
         for ph, lc in zip(self.phs, self.lcs):
             et = float(ph._aux.loc[:, 'exptime'].mean())
-            lc.downsample_nbins(nbins)
-        self.lpf = StudentLSqLPF(target, self.lcs, self.pbs)
+            lc.downsample_time(etime)
 
-        self.de_minimum = None
-        self.gpfs = None
+        self.lmlpf = StudentLSqLPF(target, self.lcs, self.pbs)
+        self.gplpf = GPLPF(target, self.lcs, self.pbs)
 
-    def optimize_global(self, niter=100):
-        self.lpf.optimize_global(niter)
-        self.de_minimum = self.lpf.de.minimum_location
+        self.lm_pv = None
+        self.gp_pv = None
+
+
+    def print_ptp_scatter(self):
+        r1s = [res.std() for res in self.gplpf.residuals(self.gp_pv)]
+        r2s = [(res - pre).std() for res, pre in zip(self.gplpf.residuals(self.gp_pv), self.gplpf.predict(self.gp_pv))]
+        for r1, r2, pb in zip(r1s, r2s, 'g r i z'.split()):
+            print('{} {:5.0f} ppm -> {:5.0f} ppm'.format(pb, 1e6 * r1, 1e6 * r2))
+
+
+    def optimize_linear_model(self, niter=100):
+        self.lmlpf.optimize_global(niter, label='Optimizing linear model')
+        self.lm_pv = self.lmlpf.de.minimum_location
+
+    def optimize_gp_model(self, niter=100):
+        self.gplpf.optimize_global(niter, label='Optimizing GP model')
+        self.gp_pv = self.gplpf.de.minimum_location
+
+    def sample_gp_model(self, niter=100, thin=5):
+        self.gplpf.sample_mcmc(niter, thin=thin, label='Sampling GP model')
 
     def mask_flux_outliers(self, sigma=4):
-        assert self.de_minimum is not None, "Need to run global optimization before calling outlier masking"
-        self.lpf.mask_outliers(sigma=sigma,
-                               means=self.lpf.compute_lc_model(self.de_minimum))
+        assert self.lm_pv is not None, "Need to run global optimization before calling outlier masking"
+        self.lmlpf.mask_outliers(sigma=sigma, means=self.lmlpf.flux_model(self.lm_pv))
 
-    def learn_gp_hyperparameters(self, max_pts=600, kernelf=None, kernelfarg=(), covariates=(2,3,4,5,6)):
-        transits = self.lpf.compute_transit(self.de_minimum)
-        self.gpfs = [M2GP(self.lpf, transits, pb, max_pts=max_pts, kernelf=kernelf, kernelfarg=kernelfarg, covariates=covariates) for pb in range(4)]
-        for gpf in tqdm(self.gpfs):
-            gpf.optimize_hps()
-        self.gp_predictions = [gpf.predict() for gpf in self.gpfs]
 
-    def plot_global_optimization(self):
-        models = [self.lpf.compute_lc_model(pv) for pv in self.lpf.de.population]
-        fig, axs = subplots(2, 2, figsize=(11, 8), sharex=True, sharey=True)
-        for i, (ax, time, flux) in enumerate(zip(axs.flat, self.lpf.times, self.lpf.fluxes)):
-            ax.plot(time, flux, '.', alpha=0.5)
-            for model in models:
-                ax.plot(time, model[i], 'k', alpha=0.1)
-        [setp(ax, title=flt) for flt, ax in zip('g r i z'.split(), axs.flat)];
-        setp(axs[0, :], xlabel='')
-        setp(axs[:, 1], ylabel='')
-        setp(axs[:, 0], ylabel='Normalized flux')
+    def learn_gp_hyperparameters(self):
+        assert self.lm_pv is not None, 'Must carry out linear model optimisation before GP hyperparameter optimisation'
+        self.gplpf.optimize_hps(self.lm_pv)
+
+    def posterior_samples(self, nsteps=0, model='gp', include_ldc=False):
+        assert model in ('linear', 'gp')
+        lpf = self.lmlpf if model == 'linear' else self.gplpf
+        fc = lpf.sampler.chain[:,-nsteps:,:].reshape([-1, lpf.de.n_par])
+        if include_ldc:
+            return pd.DataFrame(fc, columns=lpf.ps.names)
+        else:
+            return pd.DataFrame(fc[:,:5], columns=lpf.ps.names[:5])
+
+    def plot_mcmc_chains(self, pid=0, model='gp', alpha=0.1):
+        assert model in ('linear', 'gp')
+        lpf = self.lmlpf if model == 'linear' else self.gplpf
+        fig, ax = subplots()
+        ax.plot(lpf.sampler.chain[:,:,pid].T, 'k', alpha=alpha)
         fig.tight_layout()
+        return fig
 
-    def plot_light_curve(self, detrend_obs=False, detrend_mod=True, include_mod=True, figsize=(11, 6)):
-        lpf = self.lpf
-        if detrend_obs:
-            lc_baseline = lpf.compute_baseline(lpf.de.minimum_location)
-            fluxes_obs = [lpf.fluxes[i] / lc_baseline[i] for i in range(4)]
-        else:
-            fluxes_obs = lpf.fluxes
+    def plot_light_curve(self, model='linear', method='de', detrend_obs=False, detrend_mod=True, include_mod=True,
+                         figsize=(11, 6)):
+        assert model in ('linear', 'gp')
+        assert method in ('de', 'mcmc')
 
-        if detrend_mod:
-            fluxes_mod = lpf.compute_transit(lpf.de.minimum_location)
+        lpf = self.lmlpf if model == 'linear' else self.gplpf
+        posterior_limits = None
+
+        if method == 'de':
+            pv = lpf.de.minimum_location
         else:
-            fluxes_mod = lpf.compute_lc_model(lpf.de.minimum_location)
+            assert lpf.sampler is not None
+            niter = lpf.sampler.chain.shape[1]
+            fc = lpf.sampler.chain[:, niter // 2:, :].reshape([-1, lpf.de.n_par])
+            fms = [lpf.flux_model(pv) for pv in permutation(fc)[:100]]
+            fms = [array([fm[i] for fm in fms]) for i in range(4)]
+            posterior_limits = [percentile(fm, [16, 84, 0.5, 99.5], 0) for fm in fms]
+            pv = median(fc, 0)
+
+        if model == 'linear':
+            baseline = lpf.baseline(pv)
+            fluxes_obs = [lpf.fluxes[i] / baseline[i] for i in range(4)] if detrend_obs else lpf.fluxes
+            fluxes_mod = lpf.transit_model(pv) if detrend_mod else lpf.flux_model(pv)
+        else:
+            baseline = lpf.predict(pv)
+            fluxes_obs = [lpf.fluxes[i] - baseline[i] for i in range(4)] if detrend_obs else lpf.fluxes
+            fluxes_mod = lpf.transit_model(pv) if detrend_mod else [fm + bl for fm, bl in zip(lpf.transit_model(pv), baseline)]
 
         fig, axs = subplots(2, 2, figsize=figsize, sharex=True, sharey=True)
-        for i in range(4):
-            axs.flat[i].plot(lpf.times[i], fluxes_obs[i], 'k.')
+        for i, ax in enumerate(axs.flat):
+            ax.plot(lpf.times[i], fluxes_obs[i], 'k.', alpha=0.25)
             if include_mod:
                 omin = fluxes_obs[i].min()
                 rmax = (fluxes_obs[i] - fluxes_mod[i]).max()
                 shift = omin - rmax
-                axs.flat[i].plot(lpf.times[i], fluxes_mod[i], 'w-', lw=3)
-                axs.flat[i].plot(lpf.times[i], fluxes_mod[i], '-', lw=1)
-                axs.flat[i].plot(lpf.times[i], fluxes_obs[i] - fluxes_mod[i] + shift, 'k.')
+                if not posterior_limits:
+                    ax.plot(lpf.times[i], fluxes_mod[i], 'w-', lw=3)
+                ax.plot(lpf.times[i], fluxes_mod[i], 'k-', lw=1)
+                ax.plot(lpf.times[i], fluxes_obs[i] - fluxes_mod[i] + shift, 'k.', alpha=0.25)
+                if posterior_limits:
+                    ax.fill_between(lpf.times[i], posterior_limits[i][2], posterior_limits[i][3], facecolor='k',
+                                    alpha=0.3)
+                    ax.fill_between(lpf.times[i], posterior_limits[i][0], posterior_limits[i][1], facecolor='k',
+                                    alpha=0.3)
+        setp(axs, xlim=lpf.times[0][[0, -1]])
         fig.tight_layout()
+        return fig
+
 
     def plot_noise(self, xtype='time'):
         binf = arange(1, 50)
-        residuals = [gpf.residuals for gpf in self.gpfs]
-        prediction = self.gp_predictions
+        residuals = self.gplpf.residuals(self.lm_pv)
+        prediction = self.gplpf.predict(self.lm_pv)
+        exptime = diff(self.lcs[0].btime).mean()
 
         fig, axs = subplots(4, 2, figsize=(11, 6), sharey='row', sharex='col',
                             gridspec_kw={'height_ratios': [0.7, 0.3, 0.7, 0.3]})
@@ -96,11 +146,11 @@ class TransitAnalysis:
         for i, (res, pre) in enumerate(zip(residuals, prediction)):
             nbins = floor(res.size / binf)
             totp = (binf * nbins).astype(np.int)
-            s0 = 1e6 * array([res[:tp].reshape([-1, nb]).mean(1).std() for nb, tp in zip(binf, totp)])
-            s1 = 1e6 * array([(res - pre)[:tp].reshape([-1, nb]).mean(1).std() for nb, tp in zip(binf, totp)])
-            ymin = min(ymin, min(s0.min(), s1.min()))
-            ymax = max(ymax, max(s0.max(), s1.max()))
-            x = binf * 15 / 60 if xtype == 'time' else sqrt(binf)
+            s0 = 1e6 * array([mad_std(res[:tp].reshape([-1, nb]).mean(1)) for nb, tp in zip(binf, totp)])
+            s1 = 1e6 * array([mad_std((res - pre)[:tp].reshape([-1, nb]).mean(1)) for nb, tp in zip(binf, totp)])
+            ymin = min([ymin, min([s0.min(), s1.min()])])
+            ymax = max([ymax, max([s0.max(), s1.max()])])
+            x = binf * exptime * 24 * 60 if xtype == 'time' else sqrt(binf)
             axs[::2].flat[i].plot(x, s0)
             axs[::2].flat[i].plot(x, s1)
             axs[1::2].flat[i].plot(x, s0 - s1)
@@ -109,4 +159,22 @@ class TransitAnalysis:
         setp(axs[1::2, 0], ylabel='$\Delta$ STD [ppm]')
         setp(axs[-1, :], xlabel=xlabel)
         fig.tight_layout()
+        return fig
 
+
+    def save(self):
+        delm = xa.DataArray(self.lmlpf.de.population, dims='pvector lm_parameter'.split(),
+                            coords={'lm_parameter': self.lmlpf.ps.names})
+        degp = xa.DataArray(self.gplpf.de.population, dims='pvector gp_parameter'.split(),
+                            coords={'gp_parameter': self.gplpf.ps.names})
+        gphp = xa.DataArray(self.gplpf.gphps, dims='filter gp_hyperparameter'.split(),
+                            coords={'filter': 'g r i z'.split(),
+                                    'gp_hyperparameter': 'mean sky airmass xy_amplitude xy_scale entropy'.split()})
+        gpmc = xa.DataArray(self.gplpf.sampler.chain, dims='pvector step gp_parameter'.split(),
+                            coords={'gp_parameter': self.gplpf.ps.names},
+                            attrs={'ndim': self.gplpf.de.n_par, 'npop': self.gplpf.de.n_pop})
+        ds = xa.Dataset(data_vars={'de_population_lm': delm, 'de_population_gp': degp,
+                                   'gp_hyperparameters': gphp, 'gp_mcmc': gpmc},
+                        attrs={'created': strftime('%Y-%m-%d %H:%M:%S'), 'obsnight':self.ddata.absolute().name,
+                               'tid':self.tid, 'cids':self.cids, 'target':self.target})
+        ds.to_netcdf('{}_{}_fit.nc'.format(self.target, self.ddata.absolute().name))
