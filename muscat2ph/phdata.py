247c27c4ad82b3f3a902c0d9ce217aeedfeb5d43
Hannu Parviainen <hannu@iac.es>
2018-07-13 14:41:33 +0100

    Added a ReferenceStarSet class and made BJD_TDB as the main time format for PhotometryData.

--- muscat2ph/phdata.py
+++ muscat2ph/phdata.py
@@ -1,7 +1,8 @@
 import xarray as xa
+from astropy.coordinates import SkyCoord, FK5
 from matplotlib.mlab import normpdf
 
-from numpy import inf, sqrt, dot, exp, linspace, log, zeros, array, arange, meshgrid, ones, r_
+from numpy import inf, sqrt, dot, exp, linspace, log, zeros, array, arange, meshgrid, ones, r_, isin
 import patsy
 from numpy.linalg import lstsq
 from photutils import CircularAperture
@@ -9,14 +10,82 @@ from scipy.interpolate import interp1d
 from astropy.time import Time
 from astropy import coordinates as coord, units as u
 from astropy.stats import mad_std
+from astroquery.simbad import Simbad
+from tqdm import tqdm
 
 lapalma = coord.EarthLocation.from_geodetic(-17.8799*u.deg, 28.758*u.deg, 2327*u.m)
 
+N = lambda a: a/a.median('mjd')
+
+
+class ReferenceStarSet:
+    def __init__(self, ph):
+        self.ph = ph
+        self.tid = ph.tid
+        self.sc = None
+        self.tap = None
+        self.cids = []
+        self.caps = []
+        self.ptps = []
+
+    def select_best(self, min_apt=0, max_apt=4, max_cid=5, n=1):
+        N = lambda a: a / a.median('mjd')
+
+        self.tap, self.cids, self.caps, self.ptps = None, [], [], []
+        cids = arange(max_cid)
+        apts = arange(min_apt, max_apt)
+        for i in tqdm(range(n), desc='Optimising comparison stars'):
+            cids = cids[isin(cids, self.cids + [self.tid], invert=True)]
+            sc = xa.DataArray(zeros([cids.size, apts.size, apts.size]),
+                              dims='cstar tapt capt'.split(),
+                              coords={'cstar': cids, 'tapt': apts, 'capt': apts})
+
+            for icid, cid in enumerate(cids):
+                for iaid, aid in enumerate(apts):
+                    ft = self.ph.flux[:, self.tid, aid]
+                    fc = self.reference_flux + self.ph.flux.loc[:, cid][:, apts]
+                    sc[icid, iaid, :] = (self.ptp_scatter(ft / fc, 1) / sqrt(2)
+                                         + self.ptp_scatter(ft / fc, 2) / sqrt(6)
+                                         + self.ptp_scatter(ft / fc, 3) / sqrt(20))
+
+            s = sc.where(sc == sc.min(), drop=True)
+            self.sc = sc
+            if self.tap is None:
+                self.tap = int(s.tapt)
+            self.cids.append(int(s.cstar))
+            self.caps.append(int(s.capt))
+            self.ptps.append(float(s))
+        return s
+
+    @staticmethod
+    def ptp_scatter(flux, n=1):
+        return array((flux / flux.median('mjd')).diff('mjd', n=n).std('mjd') * sqrt(2))
+
+    @staticmethod
+    def std(flux):
+        return array((flux / flux.median('mjd')).std('mjd'))
+
+    @property
+    def reference_flux(self):
+        f = 0.
+        for cid, aid in zip(self.cids, self.caps):
+            f += self.ph.flux.loc[:, cid][:, aid]
+        return f
+
+    @property
+    def normalized_relative_flux(self):
+        return N(self.ph.flux[:, self.tid, self.tap] / self.reference_flux)
+
+
+
 class PhotometryData:
-    def __init__(self, fname, tid, cids, fstart=0, fend=inf):
+    def __init__(self, fname, tid, cids, fstart=0, fend=inf, objname=None, **kwargs):
         with xa.open_dataset(fname) as ds:
             self._ds = ds.load()
         self._flux = self._ds.flux
+        self._mjd = Time(self._ds.aux.loc[:,'mjd'], format='mjd', scale='utc', location=lapalma)
+        self.objname = objname
+
         self._aux = self._ds.aux
         self._cnt = self._ds.centroid
         self._sky = self._ds.sky_median
@@ -29,6 +98,8 @@ class PhotometryData:
         self.cids = cids
         self.fstart = max(0, fstart)
         self.fend = min(self.nframes, fend)
+        self.tstart = kwargs.get('tstart', -inf)
+        self.tend = kwargs.get('tend', inf)
         self.iapt = -1
         self.apt = self._flux.aperture[self.iapt]
         self.lin_formula = 'mjd + sky + xshift + yshift + entropy + airmass'
@@ -36,11 +107,15 @@ class PhotometryData:
         self._fmask = ones(self.nframes, 'bool')
         self._fmask[:self.fstart] = 0
         self._fmask[self.fend:] = 0
+        self._fmask[self._mjd.value < self.tstart] = 0
+        self._fmask[self._mjd.value > self.tend] = 0
         self._fmask &= self._flux.notnull().any(['star','aperture'])
         self._fmask = array(self._fmask)
 
         self._entropy_table = None
         self._calculate_effective_fwhm()
+        self._rset = ReferenceStarSet(self)
+        self._rset.select_best(n=4)
 
 
     def _calculate_effective_fwhm(self):
@@ -82,7 +157,7 @@ class PhotometryData:
     def aux(self):
         return xa.DataArray(list(map(array, [self.mjd.value, self.sky, self.airmass, self.xshift, self.yshift, self.entropy])),
                      name='aux', dims='quantity mjd'.split(),
-                     coords={'mjd': self._ds.mjd[self.fstart:self.fend],
+                     coords={'mjd': self.mjd.value,
                              'quantity': 'mjd sky airmass xshift yshift entropy'.split()}).T
 
     @property
@@ -91,7 +166,7 @@ class PhotometryData:
 
     @property
     def mjd(self):
-        return Time(self._aux.loc[self._fmask,'mjd'], format='mjd', scale='utc', location=lapalma)
+        return self._mjd[self._fmask]
 
     @property
     def jd(self):
@@ -99,7 +174,10 @@ class PhotometryData:
 
     @property
     def bjd(self):
-        return self.mjd.jd
+        obj = Simbad.query_object(self.objname)
+        sc = SkyCoord(obj['RA'][0], obj['DEC'][0], frame=FK5, unit=(u.hourangle, u.deg))
+        bjd = self.mjd.tdb + self.mjd.light_travel_time(sc)
+        return bjd.jd
 
     @property
     def airmass(self):
@@ -127,8 +205,13 @@ class PhotometryData:
 
     @property
     def relative_flux(self):
-        f = self.flux[:, self.tid, self.iapt] / self.flux[:, self.cids, self.iapt].sum('star')
-        return f / f.median('mjd')
+        """Normalized relative flux"""
+        return self._rset.normalized_relative_flux
+
+    @property
+    def relative_error(self):
+        """Normalized relative uncertainty"""
+        return sqrt(abs(1/self.flux[:, self._rset.tid, self._rset.tap]) + abs(1/self._rset.reference_flux))
 
     @property
     def relative_fluxes(self):
