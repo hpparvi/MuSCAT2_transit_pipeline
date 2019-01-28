1775f6ca33a989fc127e1f31630f4fbc875b94a9
Hannu Parviainen <hannu@iac.es>
2019-01-28 19:16:01 +0000

    Huge number of random undocumented improvements and fixes.

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+#  MuSCAT2 photometry and transit analysis pipeline
+#  Copyright (C) 2019  Hannu Parviainen
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from setuptools import setup, find_packages
 
 setup(name='MuSCAT2TA',
@@ -8,7 +24,7 @@ setup(name='MuSCAT2TA',
       author_email='hpparvi@gmail.com',
       url='https://github.com/hpparvi/muscat2ta',
       packages=find_packages(),
-      install_requires=["numpy", "scipy>=0.16", "astropy", "tqdm", "traitlets", 'pandas'],
+      install_requires=["numpy", "scipy>=0.16", "astropy", "tqdm", "traitlets", 'pandas', 'xarray', 'photutils', 'matplotlib', 'astroquery'],
       include_package_data=True,
       scripts=['bin/m2organize', 'bin/m2photometry', 'bin/m2astrometry', 'bin/m2fit', 'bin/m2tfop'],
       license='GPLv2',
