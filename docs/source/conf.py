ee5121ae8d5fd9db33a8e54d8389c6534cd4a79c
Hannu Parviainen <hannu@iac.es>
2020-09-14 10:32:00 +0100

    Worked on the documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,6 @@ author = 'Hannu Parviainen'
 # The full version, including alpha/beta/rc tags
 release = '0.9'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -41,6 +40,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
